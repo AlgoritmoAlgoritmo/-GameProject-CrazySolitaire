bb2d3edae11c3da86a31e9f9be63d9a45ce9cd5c
Algoritmo <irisbermudez.contacto@gmail.com>
2024-03-08 14:35:09 -0300

    SpiderGameModeTest
    - Started to work in the mock
    - Wrote and passed SpiderGameModeMock_InheritsFromSpiderGameMode successfully.
    - Wrote andpassed SpiderGameMode_InheritsFromAbstractGameMode successfully

---
+++ CrazySolitaire/Assets/Scripts/Code/Solitaire/Gameplay/Spider/SpiderGameModeMock.cs
@@ -0,0 +1,32 @@
+﻿/*
+* Author:	Iris Bermudez
+* Date:		08/03/2024
+*/
+
+
+
+using Solitaire.Gameplay.Spider;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+namespace Tests.Solitaire.Gameplay.Spider {
+    public class SpiderGameModeMock : SpiderGameMode {
+        #region Variables
+        #endregion
+
+
+        #region Public methods
+        public void SetSpiderGameMode(SpiderGameMode _spiderGameMode) {
+
+        }
+        #endregion
+
+
+        #region Mocked SpiderGameMode private methods
+
+        #endregion
+    }
+}
--- CrazySolitaire/Assets/Scripts/Tests/Solitaire/Gameplay/Spider/SpiderGameModeTest.cs
+++ CrazySolitaire/Assets/Scripts/Tests/Solitaire/Gameplay/Spider/SpiderGameModeTest.cs
@@ -13,32 +13,38 @@ using UnityEditor;
 using UnityEngine;
 using Solitaire.Gameplay.Spider;
 using Solitaire.Gameplay;
+using Solitaire.Gameplay.Cards;
 
 
 
 namespace Tests.Solitaire.Gameplay.Spider {
     public class SpiderGameModeTest {
         #region Variables
-        private GameObject spiderGameModeGameObject;
-        private SpiderGameMode spiderGameMode;
+        private GameObject spiderGameModeMockGameObject;
+        private SpiderGameModeMock spiderGameModeMock;
 
-        private const string SPIDERGAMEMODE_PREFAB_PATH = "Assets/Prefabs/Gameplay/GameMode.prefab";
+        private const string SPIDERGAMEMODEMOCK_PREFAB_PATH = "Assets/Scripts/Tests/Solitaire/"
+                                                + "Gameplay/Spider/SpiderGameModeMock Prefab.prefab";
+        private const string CARD_PREFAB_PATH = "Assets/Prefabs/Gameplay/Card Prefab.prefab";
+        private const string SPIDERCARDCONTAINERM_PREFAB_PATH = "Assets/Prefabs/Gameplay/Spider"
+                                                + "/SpiderCardContainer 6Cards.prefab";
         #endregion
 
 
         #region Tests set up
         [SetUp]
         public void Setup() {
-            spiderGameModeGameObject = GameObject.Instantiate( AssetDatabase.LoadAssetAtPath<GameObject>(
-                                                                            SPIDERGAMEMODE_PREFAB_PATH));
-            if (!spiderGameModeGameObject) {
-                throw new NullReferenceException($"GameObject at {SPIDERGAMEMODE_PREFAB_PATH} "
+            spiderGameModeMockGameObject = GameObject.Instantiate( AssetDatabase
+                                                            .LoadAssetAtPath<GameObject>(
+                                                                SPIDERGAMEMODEMOCK_PREFAB_PATH));
+            if (!spiderGameModeMockGameObject) {
+                throw new NullReferenceException($"GameObject at {SPIDERGAMEMODEMOCK_PREFAB_PATH} "
                                                     + "could not be loaded.");
             }
 
-            spiderGameMode = spiderGameModeGameObject.GetComponent<SpiderGameMode>();
-            if (!spiderGameMode) {
-                throw new NullReferenceException($"GameObject at {SPIDERGAMEMODE_PREFAB_PATH} "
+            spiderGameModeMock = spiderGameModeMockGameObject.GetComponent<SpiderGameModeMock>();
+            if (!spiderGameModeMock) {
+                throw new NullReferenceException($"GameObject at {SPIDERGAMEMODEMOCK_PREFAB_PATH} "
                                                     + "does not contain a SpiderGameMode component.");
             }
         }
@@ -47,9 +53,55 @@ namespace Tests.Solitaire.Gameplay.Spider {
 
         #region Tests
         [Test]
-        public void SpiderGameMode_IsAbstractGameMode() {
+        public void SpiderGameModeMock_InheritsFromSpiderGameMode() {
+            Assert.IsInstanceOf(typeof(SpiderGameMode), spiderGameModeMock,
+                                "SpiderGameModeMock must inherit from SpiderGameMode instance.");
+        }
+
+
+        [Test]
+        public void SpiderGameMode_InheritsFromAbstractGameMode() {
+            // Instantiate SpiderGameMode object
+            SpiderGameMode spiderGameMode = GameObject.Instantiate(new GameObject())
+                                                            .AddComponent<SpiderGameMode>();
+
+            // Assert SpiderGameMode inherits from AbstractGameMode
             Assert.IsInstanceOf(typeof(AbstractGameMode), spiderGameMode,
-                                "SpiderGameMode must inherit from AbstractGameMode.");
+                                "spiderGameMode must be a AbstractGameMode instance.");
+        }
+
+
+        [Test]
+        public void WhenInitializing_ThenDistributeCardsToContainersPropperly() {
+            // Create list of cards for SpiderGameMode initialization
+            GameObject cardFacadePrefabGameObject = GameObject.Instantiate(
+                                AssetDatabase.LoadAssetAtPath<GameObject>(CARD_PREFAB_PATH) );
+            CardFacade cardFacadePrefab = cardFacadePrefabGameObject.GetComponent<CardFacade>();
+            List<CardFacade> listOfCardsToAdd = new List<CardFacade>() {
+                                        GameObject.Instantiate( cardFacadePrefabGameObject )
+                                                                    .GetComponent<CardFacade>(),
+                                        GameObject.Instantiate( cardFacadePrefabGameObject )
+                                                                    .GetComponent<CardFacade>(),
+                                        GameObject.Instantiate( cardFacadePrefabGameObject )
+                                                                    .GetComponent<CardFacade>(),
+                                        GameObject.Instantiate( cardFacadePrefabGameObject )
+                                                                    .GetComponent<CardFacade>(),
+                                        GameObject.Instantiate( cardFacadePrefabGameObject )
+                                                                    .GetComponent<CardFacade>(),
+                                        GameObject.Instantiate( cardFacadePrefabGameObject )
+                                                                    .GetComponent<CardFacade>()
+                                    };
+            int amountOfCardsToBeAdded = listOfCardsToAdd.Count;
+
+            // Check amount of cards before initialization
+
+
+            // Initialize SpiderGameMode
+
+
+            // Check amount of distributed cards after initialization
+
+
         }
         #endregion
     }

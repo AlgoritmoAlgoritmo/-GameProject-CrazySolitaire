2d61f110ec93775bf44aa0e1e64bfcc09235832e
Algoritmo <irisbermudez.contacto@gmail.com>
2024-03-20 13:17:49 -0300

    Finished to add functionality to menu bar buttons

---
+++ CrazySolitaire/Assets/Scripts/Code/Misc/ApplicationController.cs
@@ -0,0 +1,35 @@
+﻿/*
+* Author:	Iris Bermudez
+* Date:		18/03/2024
+*/
+
+
+
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace Misc {
+    public class ApplicationController : MonoBehaviour {
+        #region Public methods
+        public void QuitApplication() {
+            Application.Quit();
+        }
+
+        public void ToggleScreenMode() {
+            if ( Screen.fullScreenMode == FullScreenMode.ExclusiveFullScreen)
+                Screen.fullScreenMode = FullScreenMode.Windowed;
+            else
+                Screen.fullScreenMode = FullScreenMode.ExclusiveFullScreen;
+        }
+
+        public void RestartCurrentScene() {
+            SceneManager.LoadScene( SceneManager.GetActiveScene().name );
+        }
+        #endregion
+
+
+        #region Private methods
+
+        #endregion
+    }
+}

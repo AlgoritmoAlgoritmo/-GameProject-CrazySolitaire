5035ffdb02f820682aaf65700a3e12362581aa3d
Algoritmo <irisbermudez.contacto@gmail.com>
2024-03-28 21:05:36 -0300

    Wrote card movement animation logic. Now I need to implement it to the gameplay code

--- CrazySolitaire/Assets/Scripts/Code/Solitaire/Feedbacks/CardMovementAnimationICommand.cs
+++ CrazySolitaire/Assets/Scripts/Code/Solitaire/Feedbacks/CardMovementAnimationICommand.cs
@@ -8,17 +8,59 @@
 using UnityEngine;
 using Misc.Command;
 using System.Threading.Tasks;
+using System.Collections;
+
+
 
 namespace Solitaire.Feedbacks {
     public class CardMovementAnimationICommand : ICommand {
         #region Variables
+        private Transform transformToMove;
+        private Vector3 targetPosition;
+        #endregion
 
+
+        #region Constructors
+        public CardMovementAnimationICommand( Transform _transformToMove, 
+                                                Vector3 _targetPosition  ) {
+            transformToMove = _transformToMove;
+            targetPosition = _targetPosition;
+        }
         #endregion
 
 
         #region Public methods
-        public Task Execute() {
-            throw new System.NotImplementedException();
+        public async Task Execute() {
+            bool isRunning = true;
+            float speed = .01f;
+            Vector3 positionDiff = targetPosition - transformToMove.position;
+
+
+            while (isRunning) {
+                transformToMove.position += positionDiff * speed;
+
+                if (transformToMove.position == targetPosition)
+                    isRunning = false;
+
+                await Task.Yield();
+            }
+        }
+
+
+        public IEnumerator MovementAnimation( Vector3 _targetPosition, Transform _transformToMove ) {
+            bool isRunning = true;
+            float speed = .01f;
+            Vector3 positionDiff = _targetPosition - _transformToMove.position;
+
+
+            while( isRunning ) {
+                _transformToMove.position += positionDiff * speed;
+
+                if (_transformToMove.position == _targetPosition)
+                    isRunning = false;
+
+                yield return null;
+            }
         }
         #endregion
 
---
+++ CrazySolitaire/Assets/Scripts/MovementTest.cs
@@ -0,0 +1,43 @@
+﻿/*
+* Author:	Iris Bermudez
+* Date:		28/03/2024
+*/
+
+
+
+using Misc.Command;
+using Solitaire.Feedbacks;
+using UnityEngine;
+
+
+
+public class MovementTest : MonoBehaviour {
+    #region Variables
+    public Transform transformToMove;
+    public Transform targetPositionTransform;
+    private CardMovementAnimationICommand command;
+    #endregion
+
+
+    #region Inherited methods
+    private void Awake() {
+        command = new CardMovementAnimationICommand( transformToMove, 
+                                                    targetPositionTransform.position );
+    }
+
+
+    private void Update() {
+        if( Input.GetKeyUp(KeyCode.S) ) {
+            Debug.Log("Starting movement.");
+            // StartCoroutine( command.MovementAnimation( targetPositionTransform.position,
+            //                                            transformToMove) );
+            GenericCommandQueue.Instance.AddCommand(command);
+        }
+    }
+    #endregion
+
+
+    #region Private methods
+
+    #endregion
+}

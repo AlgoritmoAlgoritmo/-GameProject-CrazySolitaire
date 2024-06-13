f853c913dfa8182c930187a7b714318befe72edb
Iris <31053695+AlgoritmoAlgoritmo@users.noreply.github.com>
2024-06-12 22:42:37 -0300

    Started to work in klondike game mode

--- CrazySolitaire/Assets/Scripts/Code/Solitaire/GameModes/Klondike/KlondikeGameMode.cs
+++ CrazySolitaire/Assets/Scripts/Code/Solitaire/GameModes/Klondike/KlondikeGameMode.cs
@@ -6,35 +6,39 @@
 
 
 using UnityEngine;
-using Solitaire.Gameplay;
 using System.Collections.Generic;
+using Solitaire.Gameplay;
 using Solitaire.Gameplay.Cards;
 
-public class KlondikeGameMode : AbstractGameMode {
-    #region Variables
-
-    #endregion
 
 
-    #region Public methods
-    public override void Initialize( List<CardFacade> _cards ) {
-        throw new System.NotImplementedException();
+namespace Solitaire.GameModes.Klondike {
+    public class KlondikeGameMode : AbstractGameMode {
+        #region Variables
+
+        #endregion
+
+
+        #region Public methods
+        public override void Initialize(List<CardFacade> _cards) {
+            throw new System.NotImplementedException();
+        }
+
+        #endregion
+
+
+        #region Protected methods
+        protected override bool CanBeChildOf(CardFacade _card, CardFacade _potentialParent) {
+            throw new System.NotImplementedException();
+        }
+
+        protected override bool CanCardBeDragged(CardFacade _card) {
+            throw new System.NotImplementedException();
+        }
+
+        protected override void ManageCardEvent(CardFacade _placedCard, GameObject _detectedGameObject) {
+            throw new System.NotImplementedException();
+        }
+        #endregion
     }
-
-    #endregion
-
-
-    #region Protected methods
-    protected override bool CanBeChildOf(CardFacade _card, CardFacade _potentialParent) {
-        throw new System.NotImplementedException();
-    }
-
-    protected override bool CanCardBeDragged(CardFacade _card) {
-        throw new System.NotImplementedException();
-    }
-
-    protected override void ManageCardEvent(CardFacade _placedCard, GameObject _detectedGameObject) {
-        throw new System.NotImplementedException();
-    }
-    #endregion
 }

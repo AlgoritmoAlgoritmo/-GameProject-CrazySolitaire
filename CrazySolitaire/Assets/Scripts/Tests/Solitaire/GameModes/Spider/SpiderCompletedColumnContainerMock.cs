71099be840e21b013f6be90490788f1f0491ccf7
Iris <31053695+AlgoritmoAlgoritmo@users.noreply.github.com>
2024-07-10 16:16:22 -0300

    Worked on SpiderCompletedColumnContainerTest
    Wrote and passed SpiderCompletedColumnContainerMock_Is_SpiderCompletedColumnContainer successfully.

---
+++ CrazySolitaire/Assets/Scripts/Tests/Solitaire/GameModes/Spider/SpiderCompletedColumnContainerMock.cs
@@ -0,0 +1,31 @@
+﻿/*
+* Author:	Iris Bermudez
+* Date:		10/07/2024
+*/
+
+
+
+using UnityEngine;
+using Solitaire.GameModes.Spider;
+
+
+
+namespace Tests.Solitaire.GameModes.Spider {
+	public class SpiderCompletedColumnContainerMock : SpiderCompletedColumnContainer {
+		#region Variables
+
+		#endregion
+
+
+		#region Public methods
+		public int GetCardsAmount() {
+			return cards.Capacity;
+		}
+		#endregion
+
+
+		#region Private methods
+
+		#endregion
+	}
+}
---
+++ CrazySolitaire/Assets/Scripts/Tests/Solitaire/GameModes/Spider/SpiderCompletedColumnContainerTest.cs
@@ -0,0 +1,55 @@
+﻿/*
+* Author:	Iris Bermudez
+* Date:		10/07/2024
+*/
+
+
+  
+using NUnit.Framework;
+using UnityEngine;
+using Solitaire.GameModes.Spider;
+
+
+
+namespace Tests.Solitaire.GameModes.Spider {
+    public class SpiderCompletedColumnContainerTest {
+        #region Variables
+        private GameObject spiderCompletedColumnContainerGameObject;
+        private SpiderCompletedColumnContainerMock spiderCompletedColumnContainerMock;
+        #endregion
+
+
+        #region Tests setup
+        [SetUp]
+        public void Setup() {
+            spiderCompletedColumnContainerGameObject = GameObject.Instantiate(
+                                                                new GameObject() );
+
+            if( !spiderCompletedColumnContainerGameObject ) {
+                throw new System.NullReferenceException( "spiderCompletedColumnContainer" 
+                                            + "GameObject could not be instantiated." );
+            }
+
+            spiderCompletedColumnContainerMock = spiderCompletedColumnContainerGameObject
+                                        .AddComponent<SpiderCompletedColumnContainerMock>();
+
+            if( !spiderCompletedColumnContainerMock ) {
+                throw new System.NullReferenceException( "Couldn't add "
+                                        + "SpiderCompletedColumnContainerMock component to "
+                                        + "spiderCompletedColumnContainerGameObject" );
+            }
+        }
+        #endregion
+
+
+        #region Tests
+        [Test]
+        public void SpiderCompletedColumnContainerMock_Is_SpiderCompletedColumnContainer() {
+            Assert.IsInstanceOf( typeof( SpiderCompletedColumnContainer ),
+                                spiderCompletedColumnContainerMock,
+                                "Make sure spiderCompletedColumnContainerMock inherits "
+                                                + "from SpiderCompletedColumnContainer" );
+        }
+        #endregion
+    }
+}
